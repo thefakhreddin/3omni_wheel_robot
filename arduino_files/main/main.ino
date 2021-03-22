f3bafd75a939f2513f08fd5870894aeba7db143e
Fakhreddin <fakhredin0098@gmail.com>
2021-03-22 15:39:43 +0430

    pins defined

--- arduino_files/main/main.ino
+++ arduino_files/main/main.ino
@@ -1,1 +1,21 @@
+#define encoder_1_a 22
+#define encoder_1_b 23
+#define encoder_2_a 24
+#define encoder_2_b 25
+#define encoder_3_a 26
+#define encoder_3_b 27
 
+#define motor_1_a   28
+#define motor_1_b   29
+#define motor_2_a   30
+#define motor_2_b   31
+#define motor_3_a   32
+#define motor_3_b   33
+
+void setup(){
+  
+}
+
+void loop(){
+  
+}
