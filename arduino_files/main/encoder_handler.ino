7af0f64a040d7016083499ed7edb8dc4733143fe
Fakhreddin <fakhredin0098@gmail.com>
2021-03-23 12:57:01 +0430

    omega read

--- arduino_files/main/encoder_handler.ino
+++ arduino_files/main/encoder_handler.ino
@@ -1,8 +1,19 @@
-void encoder_handler_1_a() {
-  if (digitalRead(encoder_1_a)) {
-    if (!digitalRead(encoder_1_b)) en_counter[0]++;
-    else en_counter[0]--;
-  } else {
+boolean update_wheel_speed(void *) {
+  for (int i = 0; i < 3; i++) {                                                   // for each wheel
+    float en_counted = abs(en_counter[i] - en_counter_old[i]);                    // determine how many paulses has elapsed
+    long en_time_elapsed = millis() - en_counter_time;                            // the sampling timing window
+    en_counter_old[i] = en_counter[i];                                            // update the counter log for each wheel
+    wheel_w[i] = ((en_counted / rotation_pls) * 2000 * PI) / en_time_elapsed;     // calculate the omega of each wheel
+  }
+  en_counter_time = millis();                                                     // log the last sampling time
+  return true;                                                                    // is necessary for the timer to resume
+}
+
+void encoder_handler_1_a() {                                                      // get called when signal "a" status changes
+  if (digitalRead(encoder_1_a)) {                                                 // if "a" is rised
+    if (!digitalRead(encoder_1_b)) en_counter[0]++;                               // if the "b" signal is low increment counter
+    else en_counter[0]--;                                                         // if the "b" signal is high decrement counter
+  } else {                                                                        // vice versa
     if (digitalRead(encoder_1_b)) en_counter[0]++;
     else en_counter[0]--;
   }
--- arduino_files/main/main.ino
+++ arduino_files/main/main.ino
@@ -1,3 +1,6 @@
+#include <PID_v1.h>
+#include <arduino-timer.h>
+
 #define encoder_1_a 22
 #define encoder_1_b 23
 #define encoder_2_a 24
@@ -12,7 +15,24 @@
 #define motor_3_a   32
 #define motor_3_b   33
 
-long en_counter[] = {0, 0, 0};
+#define motor_1_pwm 2
+#define motor_2_pwm 3
+#define motor_3_pwm 4
+
+const long rotation_pls = 750;        // number of paulses in one complete rotation
+
+long en_counter[] = {0, 0, 0};        // paulse counter for encoders
+long en_counter_old[] = {0, 0, 0};    // paulse counter for encoders log
+long en_counter_time;                 // encoders counter sampling time
+double wheel_w[] = {0, 0, 0};         // acctual omega of the wheels (rad/s)
+double motor_pwm[] = {0, 0, 0};       // controller effort
+double wheel_w_ds[] = {0, 0, 0};      // desigered omega of the wheels (rad/s)
+
+double Kp = 2, Ki = 0, Kd = 0;        // pid controller for wheels speed control
+
+auto timer = timer_create_default();  // timer object for sampling the encoder
+
+PID motor_1_speed_pid(&wheel_w[0], &motor_pwm[0], &motor_1_w_ds, Kp, Ki, Kd, DIRECT);   // pid controller init for each wheel speed control
 
 void setup() {
   Serial.begin(9600);
@@ -24,14 +44,18 @@ void setup() {
   pinMode(encoder_3_a, INPUT_PULLUP);
   pinMode(encoder_3_b, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(encoder_1_a), encoder_handler_1_a, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder_1_a), encoder_handler_1_a, CHANGE);     // quadrature encoder with a pair of intrrupts for each wheel
   attachInterrupt(digitalPinToInterrupt(encoder_1_b), encoder_handler_1_b, CHANGE);
   attachInterrupt(digitalPinToInterrupt(encoder_2_a), encoder_handler_2_a, CHANGE);
   attachInterrupt(digitalPinToInterrupt(encoder_2_b), encoder_handler_2_b, CHANGE);
   attachInterrupt(digitalPinToInterrupt(encoder_3_a), encoder_handler_3_a, CHANGE);
   attachInterrupt(digitalPinToInterrupt(encoder_3_b), encoder_handler_3_b, CHANGE);
 
+  timer.every(10, update_wheel_speed);      // timer object init
+
+  motor_1_speed_pid.SetMode(AUTOMATIC);     // pid init
 }
 
 void loop() {
+  timer.tick();    // refresh the timer
 }
